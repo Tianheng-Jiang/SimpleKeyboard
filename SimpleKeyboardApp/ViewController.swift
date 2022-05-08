51d54038f42d2d46dcb64cf9c5e5dab1bb85b00a
Peter Zhong <petertianzhong@gmail.com>
2022-05-08 23:50:46 +1200

    init keyboard project

---
+++ SimpleKeyboardApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleKeyboardApp
+//
+//  Created by Peter on 8/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
